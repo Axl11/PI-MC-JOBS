41a084688e3fe904e1209106c9679cc1e2e6711d
Jesus Eduardo Martinez Hernandez <jemhernandez3188@gmail.com>
2022-10-16 21:48:20 -0500

    bootstrap implementado y vista de tabla

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,18 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/bootstrap.css',
+                'resources/js/bootstrap1.js',
+                'resources/css/woox/animate.css',
+                'resources/css/woox/fontawesome.css',
+                'resources/css/woox/owl.css',
+                'resources/css/woox/templatemo-woox-travel.css',
+                'resources/js/woox/vendor/jquery.js',
+                'resources/js/woox/custom.js',
+                'resources/js/woox/isotope.js',
+                'resources/js/woox/owl-carousel.js',
+                'resources/js/woox/popup.js',
+                'resources/js/woox/tabs.js'
             ],
             refresh: [
                 ...refreshPaths,
