b5cbb2465d8c79d9625952a9c9e85ebad6ec6637
Bryan Axl Coronado Cabrera <axlville5@gmail.com>
2022-10-31 14:46:10 -0600

    Validation bug fixed de empresa_id

--- app/Http/Controllers/VacanteController.php
+++ app/Http/Controllers/VacanteController.php
@@ -101,7 +101,7 @@ class VacanteController extends Controller
             'direccionVacante'=>'required|min:1|max:255',
             'horarioVacante'=>'max:255',
             'puestosDisponibles'=>'integer|min:0',
-            'empresa_id' => 'required|exists:vacantes,id',
+            'empresa_id' => 'required|exists:empresas,id',
         ]);
         
         Vacante::where('id', $vacante->id)->update($request->except('_token', '_method'));
