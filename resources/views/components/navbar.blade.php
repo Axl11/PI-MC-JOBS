4d040323b0153659fdda801d89dc724434c6c151
Jesus Eduardo Martinez Hernandez <jemhernandez3188@gmail.com>
2022-11-15 10:16:31 -0600

    ModificacionVite-NuevoNavbar
    Solo se dejan los app dentro de vite para que funcione Jetstream y se mueve todo lo demás a public para que funcione el JS

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,46 +1,33 @@
 <header class="header-area header-sticky">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <nav class="main-nav">
-                    <!-- ***** Logo Start ***** -->
-                    <a href="#" class="logo">
-                        <img src="{{URL::asset('images/logomcjobs3.png')}}" alt="MC-JOBS">
-                    </a>
-                    <!-- ***** Logo End ***** -->
-                    <!-- ***** Menu Start ***** -->
-                    <ul class="nav">
-                        <li><a href="/">Inicio</a></li>
-                        <li><a href="/vacante">Vacantes</a></li>
-                        @if (Route::has('login'))
-                            @auth
-                                <li><a href="/empresa">Empresas</a></li>
-                                <li><a href="/empleado">Empleados</a></li>
-                                <li><a href="/departamento">Departamentos</a></li>
-                                <li>
-                                    <form action="{{ route('logout') }}" method="POST">
-                                        @csrf
-                                        <x-jet-dropdown-link href="{{ route('logout') }}"
-                                        onclick="event.preventDefault(); this.closest('form').submit();">
-                                            {{ __('Log Out') }}
-                                        </x-jet-dropdown-link>
-                                    </form>
-                                </li>
-                            @else
-                                <li><a href="{{ route('login') }}">Login</a></li>
-                                @if (Route::has('register'))
-                                    <li><a href="{{ route('register') }}">Registrarse</a></li>
-                                @endif
-                            @endauth
-                        @endif
-                        <!-- <li><a href="">Contacto</a></li> -->
-                    </ul>   
-                    <a class="menu-trigger">
-                        <span>Menu</span>
-                    </a>
-                    <!-- ***** Menu End ***** -->
-                </nav>
-            </div>
-        </div>
-    </div>
+    <section id="barra" class="inner-width">
+        <!-- ***** Logo Start ***** -->
+        <a href="/" class="logo">
+            <img src="{{URL::asset('images/logomcjobs3.png')}}" alt="MC-JOBS">
+        </a>
+        <i class="menu-toggle-btn fas fa-bars"></i>
+        <!-- ***** Menu Start ***** -->
+        <nav class="navigation-menu">
+            <a href="/"><i class="fas fa-home home"></i> Inicio</a>
+            <a href="/vacante"><i class="fas fa-suitcase works"></i> Vacantes</a>
+            @if (Route::has('login'))
+                @auth
+                    <a href="/empresa"><i class="fas fa-building contact"></i> Empresas</a>
+                    <a href="/empleado"><i class="fas fa-users about"></i> Empleados</a>
+                    <a href="/departamento"><i class="fas fa-user-tag contact"></i> Departamentos</a>
+                    <form action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <x-jet-dropdown-link href="{{ route('logout') }}"
+                        onclick="event.preventDefault(); this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-jet-dropdown-link>
+                    </form>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Registrarse</a>
+                    @endif
+                @endauth
+            @endif
+        </nav>
+    </section>
 </header>
