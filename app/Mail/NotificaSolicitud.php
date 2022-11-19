340e03bcdab0826075636c85941ad9fccaab4813
Jesus Eduardo Martinez Hernandez <jemhernandez3188@gmail.com>
2022-11-18 20:57:14 -0600

    Correos (Validacion y Registro Solicitud)
    Implementación de los dos tipos de correo. Para verificar correo electronico al registrarse y un correo personalizado para el registro de solicitudes (Sin diseno)

---
+++ app/Mail/NotificaSolicitud.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Solicitude;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class NotificaSolicitud extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $solicitude;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Solicitude $solicitude)
+    {
+        $this->solicitude = $solicitude;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('correos.notificaSolicitude');
+    }
+}
---
+++ resources/views/correos/notificaSolicitude.blade.php
@@ -0,0 +1,5 @@
+<h1>Notificacion de tu Solicitud</h1>
+
+<p>
+    Tu solicitud {{$solicitude->id}} ha sido registrada.
+</p>
